5d39f7e019cb2c6d9a2621d3a6d5c7646b7d1ab9
Douglch <douglchwm@gmail.com>
2025-04-09 18:14:26 +0800

    Add setup for mismatches count and respective POS

--- submission/analysis/analysis_helper.py
+++ submission/analysis/analysis_helper.py
@@ -2,6 +2,7 @@ import nltk
 from nltk.translate.meteor_score import meteor_score
 from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 from nltk.metrics.distance import edit_distance
+import jieba.posseg as posseg
 from collections import defaultdict
 import os
 import pandas as pd
@@ -59,11 +60,14 @@ def analyze_translations(en_path, zh_trans_path, zh_correct_path, save_path, mod
 
             # Tokenize the Chinese sentences
             zh_tokens = list(jieba.cut(zh_line))
-            zh_correct_tokens = list(jieba.cut(zh_correct_line))
+            zh_correct_pos = list(posseg.cut(zh_correct_line))
+            zh_correct_words = [
+                token.word for token in zh_correct_pos]  # For metrics
+            # zh_correct_tokens = list(jieba.cut(zh_correct_line))
 
             # Compute the sentence-level scores.
-            meteor = meteor_score([zh_correct_tokens], zh_tokens)
-            bleu = sentence_bleu([zh_correct_tokens],
+            meteor = meteor_score([zh_correct_words], zh_tokens)
+            bleu = sentence_bleu([zh_correct_words],
                                  zh_tokens, smoothing_function=smoothing)
             dist = edit_distance(zh_line, zh_correct_line)
 
@@ -78,13 +82,15 @@ def analyze_translations(en_path, zh_trans_path, zh_correct_path, save_path, mod
             })
 
             # Tally missing words from the reference that do not appear in the model output
-            for word in zh_correct_tokens:
+            for word in zh_correct_pos:
                 if word not in zh_tokens:
                     word_mismatches[word] += 1
 
     df_sentences = pd.DataFrame(sentence_results)
-    df_mismatches = pd.DataFrame(list(word_mismatches.items()), columns=[
-                                 'Word', 'MismatchCount'])
+    df_mismatches = pd.DataFrame(
+        [(word, pos, count) for (word, pos), count in word_mismatches.items()],
+        columns=['Word', 'POS', 'MismatchCount']
+    )
     df_mismatches.sort_values(
         by='MismatchCount', ascending=False, inplace=True)
 
